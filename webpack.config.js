a60ca40d00e2281b7bc214e8a232a83ddb776378
Andrés Largo <felipeflogxd@gmail.com>
2021-02-07 17:49:57 -0500

    assets css

--- webpack.config.js
+++ webpack.config.js
@@ -4,5 +4,13 @@ module.exports = {
   entry: './index.js',
   output: {
     path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      }
+    ]
   }
 }
