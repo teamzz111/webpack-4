6090aec3a7c96ffe2d620d431a3727c0a8d9f86c
Andrés Largo <felipeflogxd@gmail.com>
2021-02-07 19:01:17 -0500

    CSS Plugin

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
-import './index.css';
+import './css/style.css';
 
 console.log('Hola')
--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,30 @@
 const path = require('path');
+const MiniCSSExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
   entry: './index.js',
   output: {
     path: path.resolve(__dirname, 'dist')
   },
+  mode: 'production',
   module: {
     rules: [
       {
         test: /\.css$/,
-        use: ['style-loader', 'css-loader']
-      }
+        include: path.resolve(__dirname, 'css'),
+        use: [
+        {
+          loader: MiniCSSExtractPlugin.loader
+        },
+        'css-loader',
+        ]
+      },
+
     ]
-  }
+  },
+  plugins: [
+    new MiniCSSExtractPlugin({
+      filename: 'css/[name].css'
+    })
+  ]
 }
