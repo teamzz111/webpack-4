566590597f412318026bc4a7b05575c48b989742
Andrés Largo <felipeflogxd@gmail.com>
2021-02-07 19:44:46 -0500

    babel transp

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const webpack = require('webpack');
 const MiniCSSExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
@@ -6,9 +7,17 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'dist')
   },
+  devServer: {
+    hot: true
+  },
   mode: 'production',
   module: {
     rules: [
+      {
+        test: /\.js$/,
+        use: 'babel-loader', // Intercepta los archivos de js y transpila en versiones más antiguas que entiendan la mayoría de los navegadores
+        exclude: /node_modules/,
+      },
       {
         test: /\.css$/,
         include: path.resolve(__dirname, 'css'),
@@ -25,6 +34,8 @@ module.exports = {
   plugins: [
     new MiniCSSExtractPlugin({
       filename: 'css/[name].css'
-    })
+    }),
+    new webpack.HotModuleReplacementPlugin()
+
   ]
 }
